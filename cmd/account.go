3d83f7a7644fa941b4a2b6f5dd376d2b5398b368
vigneshwaran48 <p3487260@gmail.com>
2025-03-03 21:47:10 +0530

    Auth token changes

--- cmd/account.go
+++ cmd/account.go
@@ -1,6 +1,11 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/vigneshwaran-48/zshell/utils"
+)
 
 var account = &cobra.Command{
 	Use:   "account",
@@ -8,4 +13,27 @@ var account = &cobra.Command{
 	Long:  "Account operations",
 	Run: func(cmd *cobra.Command, args []string) {
 	},
+}
+
+var accountList = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all accounts",
+	Long:  "Lists all accounts",
+	Run: func(cmd *cobra.Command, args []string) {
+		dcName, err := cmd.Flags().GetString("dc")
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+		accessToken, err := utils.GetAccessToken(dcName)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+		fmt.Println(accessToken)
+	},
+}
+
+func init() {
+	account.AddCommand(accountList)
+
+	rootCmd.AddCommand(account)
 }
