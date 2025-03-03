107905bf6165a9c229378a77be9130364b92fd16
vigneshwaran48 <p3487260@gmail.com>
2025-03-03 21:51:28 +0530

    Added login command

---
+++ cmd/login.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/vigneshwaran-48/zshell/utils"
+)
+
+var login = &cobra.Command{
+	Use:   "login",
+	Short: "Login to a DC",
+	Long:  "Login to a DC",
+	Run: func(cmd *cobra.Command, args []string) {
+		dcName, err := cmd.Flags().GetString("dc")
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+		_, err = utils.LoginToDC(dcName)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(login)
+}
