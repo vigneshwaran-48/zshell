030c0a347100f6ea6dc83c32785b7f92a514b084
vigneshwaran48 <p3487260@gmail.com>
2025-03-03 22:05:07 +0530

    Using go-pretty for ouput

--- cmd/account.go
+++ cmd/account.go
@@ -1,10 +1,15 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
+	zmail "github.com/vigneshwaran-48/zmail-go-sdk"
 	"github.com/vigneshwaran-48/zshell/utils"
+	"golang.org/x/oauth2"
 )
 
 var account = &cobra.Command{
@@ -28,7 +33,35 @@ var accountList = &cobra.Command{
 		if err != nil {
 			cobra.CheckErr(err)
 		}
-		fmt.Println(accessToken)
+
+		config := zmail.NewConfiguration()
+		client := zmail.NewAPIClient(config)
+
+		token := &oauth2.Token{
+			AccessToken: accessToken,
+			TokenType:   "Bearer",
+		}
+
+		tokenSource := oauth2.StaticTokenSource(token)
+
+		ctx := context.WithValue(context.Background(), zmail.ContextOAuth2, tokenSource)
+		req := client.AccountsAPI.Getmailaccounts(ctx)
+		accountsResp, httpResp, err := req.Execute()
+		if err != nil {
+			data, err := json.Marshal(httpResp)
+			if err != nil {
+				cobra.CheckErr(err)
+			}
+			fmt.Println(string(data))
+			cobra.CheckErr(err)
+		}
+		for _, account := range accountsResp.Data {
+			data, err := json.Marshal(account)
+			if err != nil {
+				cobra.CheckErr(err)
+			}
+			fmt.Println(string(data))
+		}
 	},
 }
 
