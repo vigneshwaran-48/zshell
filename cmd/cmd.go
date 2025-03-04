6aa6943541b8960c3af3b608292d57d40bb71ccb
vigneshwaran48 <p3487260@gmail.com>
2025-03-04 22:00:10 +0530

    Remove debug code

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/reeflective/console"
 	"github.com/spf13/cobra"
 )
@@ -16,19 +14,5 @@ func GetCmds() *cobra.Command {
 func StartInteractiveShell() {
 	app = console.New("ZShell")
 	app.ActiveMenu().SetCommands(GetCmds)
-	app.Shell().Keymap.Register(map[string]func(){
-		"completion-command": func() {
-			fmt.Println("completion")
-		},
-	})
-	app.ActiveMenu().PersistentPostRun = func(cmd *cobra.Command, args []string) {
-		fmt.Println("Tess")
-		if len(args) == 0 {
-			return
-		}
-		if args[0] == "|" {
-			fmt.Println("Piping request")
-		}
-	}
 	app.Start()
 }
